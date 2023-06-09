1768f24c58f16176640a12ddde244d73642c4851
Jinu Augustine <jinu.augustine@outlook.com>
2023-06-09 11:41:22 -0400

    accept folder

--- highresimg.py
+++ highresimg.py
@@ -1,16 +1,34 @@
+import os
 from pdf2image import convert_from_path
 
-def convert_pdf_to_images(pdf_path, output_path, dpi=1200):
-    images = convert_from_path(pdf_path, dpi=dpi)
+def convert_folder_to_images(folder_path, output_path, dpi=1200):
+    # Create the output directory if it doesn't exist
+    os.makedirs(output_path, exist_ok=True)
 
-    for i, image in enumerate(images):
-        image_path = f"{output_path}/page_600dpi_{i+1}.png"
-        image.save(image_path, "PNG")
+    # Get a list of all PDF files in the folder
+    pdf_files = [file for file in os.listdir(folder_path) if file.endswith(".pdf")]
+
+    # Iterate over each PDF file
+    for pdf_file in pdf_files:
+        pdf_path = os.path.join(folder_path, pdf_file)
+
+        # Convert PDF to images
+        images = convert_from_path(pdf_path, dpi=dpi)
+
+        # Save the images
+        for i, image in enumerate(images):
+            image_path = os.path.join(output_path, f"{pdf_file}_page_{i+1}.png")
+            image.save(image_path, "PNG")
 
     print("Conversion complete!")
 
-pdf_path = "Dataset/Ai Research/128611501_GM Financial_Issued 100215 (2).PDF"  # Path to the input PDF file
-output_path = "HighRes_Images"  # Output directory to save the images
-dpi = 600  # DPI (dots per inch) for high-quality output
+# Set the input folder path containing PDF files
+folder_path = "Input_PDFs"
 
-convert_pdf_to_images(pdf_path, output_path, dpi)
+# Set the output folder path to save the images
+output_path = "Output_Images"
+
+# Set the DPI (dots per inch) for high-quality output
+dpi = 600
+
+convert_folder_to_images(folder_path, output_path, dpi)
