fd080c02b3ef2e79e983f3d71eaa57da0d58d73b
Jinu Augustine <jinu.augustine@outlook.com>
2023-06-09 12:40:15 -0400

    pdfpage error

--- highresimg.py
+++ highresimg.py
@@ -1,5 +1,6 @@
 import os
 from pdf2image import convert_from_path
+from pdf2image.exceptions import PDFPageCountError
 
 def convert_folder_to_images(folder_path, output_path, dpi=1200):
     # Create the output directory if it doesn't exist
@@ -12,15 +13,33 @@ def convert_folder_to_images(folder_path, output_path, dpi=1200):
     for pdf_file in pdf_files:
         pdf_path = os.path.join(folder_path, pdf_file)
 
-        # Convert PDF to images
-        images = convert_from_path(pdf_path, dpi=dpi)
+        try:
+            # Get the page count of the PDF file
+            page_count = get_pdf_page_count(pdf_path)
 
-        # Save the images
-        for i, image in enumerate(images):
-            image_path = os.path.join(output_path, f"{pdf_file}_page_{i+1}.png")
-            image.save(image_path, "PNG")
+            # Convert PDF to images
+            images = convert_from_path(pdf_path, dpi=dpi)
 
-    print("Conversion complete!")
+            # Save the images
+            for i, image in enumerate(images):
+                image_path = os.path.join(output_path, f"{pdf_file}_page_{i+1}.png")
+                image.save(image_path, "PNG")
+
+            print(f"Conversion complete for {pdf_file}. Pages: {page_count}")
+        except PDFPageCountError:
+            print(f"Error getting page count for {pdf_file}")
+        except Exception as e:
+            print(f"Error converting {pdf_file}: {str(e)}")
+
+    print("Conversion process complete!")
+
+def get_pdf_page_count(pdf_path):
+    try:
+        with open(pdf_path, "rb") as file:
+            pdf_data = file.read()
+            return len(pdf_data.split(b"/Type /Page")) - 1
+    except Exception as e:
+        raise PDFPageCountError(f"Error getting page count: {str(e)}")
 
 # Set the input folder path containing PDF files
 folder_path = r"Input_PDFs"
